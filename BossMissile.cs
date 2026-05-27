a30af9a94132bb9a6172f332ce9f2c69358cedd6
yungshi <81853631+yungshi@users.noreply.github.com>
2026-05-27 17:29:52 +0900

    Implement BossMissile class for targeting behavior

---
+++ BossMissile.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossMissile : Bullet
+{
+    public Transform target;
+    NavMeshAgent nav;
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(target.position);
+        
+    }
+}
