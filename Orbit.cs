51aa52451a12008960d492e82745d876c3cf2a71
yungshi <81853631+yungshi@users.noreply.github.com>
2026-05-31 00:14:33 +0900

    Update Orbit.cs

--- Orbit.cs
+++ Orbit.cs
@@ -1,17 +1,17 @@
-using UnityEngine;//*
+using UnityEngine;
 
-public class Orbit : MonoBehaviour//*
+public class Orbit : MonoBehaviour
 {
     public Transform target;
     public float orbitSpeed;
     Vector3 offSet;
 
-    void Start()
+    void Start()//타겟과 오브젝트 간격
     {
         offSet = transform.position - target.position;
     }
 
-    void Update()
+    void Update()//프래임 마다 위치 업데이트
     {
         transform.position = target.position + offSet;
         transform.RotateAround(target.position, Vector3.up, orbitSpeed * Time.deltaTime);
