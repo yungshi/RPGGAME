1ac9ff64a251fcd6b65dd6fad9d2b2caa5370751
yungshi <81853631+yungshi@users.noreply.github.com>
2026-05-30 23:33:46 +0900

    Enhance BossMissile to track player in real-time

--- BossMissile.cs
+++ BossMissile.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class BossMissile : Bullet
+public class BossMissile : Bullet //Bullet 클래스 기반
 {
     public Transform target;
-    NavMeshAgent nav;
+    NavMeshAgent nav;//길을 찾기 위해 선언
     void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
@@ -12,7 +12,7 @@ public class BossMissile : Bullet
 
     void Update()
     {
-        nav.SetDestination(target.position);
+        nav.SetDestination(target.position);//플레이어 실시간 추적
         
     }
 }
