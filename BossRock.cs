6c31056fc60a86db12d93faf09c45fdf5daf77aa
yungshi <81853631+yungshi@users.noreply.github.com>
2026-05-30 23:39:00 +0900

    Update BossRock.cs

--- BossRock.cs
+++ BossRock.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
 
-public class BossRock : Bullet
+public class BossRock : Bullet//Bullet 클래스 기반
 {
     Rigidbody rigid;
-    float angularPower = 2;
-    float scaleValue = 0.1f;
-    bool isShoot;
+    float angularPower = 2;//회전력
+    float scaleValue = 0.1f;//크기
+    bool isShoot;//False 기본
 
-    void Awake()
+    void Awake()//최초 실행동작
     {
         rigid = GetComponent<Rigidbody>();
         StartCoroutine(GainPowerTimer());
         StartCoroutine(GainPower());
     }
 
-    IEnumerator GainPowerTimer()
+    IEnumerator GainPowerTimer()//2.2 대기후 발사 루프
     {
         yield return new WaitForSeconds(2.2f);
         isShoot = true;
     }
 
-    IEnumerator GainPower()
+    IEnumerator GainPower()//크기를 커지게함
     {
         while (!isShoot)
         {
