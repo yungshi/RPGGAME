aaae07ee943deeca75f8c297548ed1d55936f7f0
yungshi <81853631+yungshi@users.noreply.github.com>
2026-05-30 23:10:02 +0900

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage;
-    public bool isMelee;
-    public bool isRock;
+    public bool isMelee;//?
+    public bool isRock;//?
     void OnCollisionEnter(Collision collision)
     {
         if(!isRock &&collision.gameObject.tag == "Floor")
@@ -14,7 +14,7 @@ public class Bullet : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)//?
     {
         if (!isMelee && other.gameObject.tag == "Wall")
         {
