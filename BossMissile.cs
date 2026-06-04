dd3b854e62741471ccd75ec2dae79d3be1d448e5
yungshi <81853631+yungshi@users.noreply.github.com>
2026-06-04 23:21:49 +0900

    Update BossMissile.cs

--- BossMissile.cs
+++ BossMissile.cs
@@ -3,7 +3,7 @@ using UnityEngine.AI;
 
 public class BossMissile : Bullet //Bullet 클래스 기반
 {
-    public Transform target;
+    public Transform target;//타겟 위치 변수
     NavMeshAgent nav;//길을 찾기 위해 선언
     void Awake()
     {
